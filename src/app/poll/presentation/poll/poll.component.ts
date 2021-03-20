dc898ccc55a40eaad01356f3dd63feaeadfd8783
Mauriverti da Silva Junior <maurivertijr@gmail.com>
2021-03-20 13:01:39 -0300

    Improve usability

--- src/app/poll/presentation/poll/poll.component.ts
+++ src/app/poll/presentation/poll/poll.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { SessionService } from 'src/app/auth/domain/services/session.service';
 import { LoginService } from 'src/app/login/domain/services/login.service';
 import { AppRoutes } from 'src/app/shared/app-routes';
 
@@ -9,13 +10,28 @@ import { AppRoutes } from 'src/app/shared/app-routes';
   templateUrl: './poll.component.html'
 })
 export class PollComponent {
+
+  isAnonymous?: boolean;
+
   constructor(
     private router: Router,
     private auth: LoginService,
-  ) { }
+    private session: SessionService,
+  ) {
+    this.isAnonymous = this.session.fetchAuthData().anonymous;
+  }
+
+  login(): void {
+    this.auth.logout();
+    this.toLogin();
+  }
 
   logout(): void {
     this.auth.logout();
+    this.toLogin();
+  }
+
+  toLogin(): void {
     this.router.navigate([AppRoutes.LOGIN]);
   }
 }
